5564c3a7c946f0999a8cd59f67519defa21db983
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2025-01-28 22:33:54 +0100

    Add ManualInteractiveMsalHostingTests

---
+++ test/FredrikHr.Hosting.Msal.TUnit/ManualInteractiveMsalHostingTests.cs
@@ -0,0 +1,46 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Client.Extensions.Msal;
+
+namespace FredrikHr.Hosting.Msal.TUnit;
+
+[ClassDataSource<TUnitHostBuilderFactory>(Shared = SharedType.PerAssembly)]
+public class ManualInteractiveMsalHostingTests(TUnitHostBuilderFactory hostBuilderFactory)
+{
+    private readonly TUnitHostBuilderFactory _hostBuilderFactory =
+        hostBuilderFactory;
+
+    [Test, Explicit]
+    public async Task PublicInteractiveUserTokenForOpenId(CancellationToken cancelToken = default)
+    {
+        var hostBuilder = _hostBuilderFactory.CreateHostAppBuilder();
+        hostBuilder.Services.AddOptions<ApplicationOptions>()
+            .BindConfiguration(ConfigurationPath.Combine("Microsoft.Identity.Client", "ApplicationOptions"));
+        hostBuilder.Services.AddOptions<PublicClientApplicationOptions>()
+            .ConfigureApplyBaseType<PublicClientApplicationOptions, ApplicationOptions>()
+            .BindConfiguration(ConfigurationPath.Combine("Microsoft.Identity.Client", "PublicClient"));
+        hostBuilder.Services.AddMsalPublicClient()
+            .ConfigureLogging()
+            .ConfigureMsalHttpFactory()
+            ;
+
+        using var host = hostBuilder.Build();
+        await host.StartAsync(cancelToken)
+            .ConfigureAwait(continueOnCapturedContext: false);
+        var serviceProvider = host.Services;
+
+        var publicMsal = serviceProvider
+            .GetRequiredService<IPublicClientApplication>();
+
+        var msalRequest = publicMsal.AcquireTokenInteractive(["openid"])
+            .WithPrompt(Prompt.SelectAccount)
+            ;
+        var msalAuthResult = await msalRequest.ExecuteAsync(cancelToken)
+            .ConfigureAwait(continueOnCapturedContext: false);
+
+        await host.StopAsync(cancelToken)
+            .ConfigureAwait(continueOnCapturedContext: false);
+    }
+}
