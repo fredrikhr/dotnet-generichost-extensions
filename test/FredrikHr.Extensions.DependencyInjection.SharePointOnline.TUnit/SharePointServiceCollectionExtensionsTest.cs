df0f303ad5abd8c9553329bf27fc60af026e79fc
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2025-02-22 16:14:25 +0100

    Rename folder and project names

--- test/FredrikHr.Extensions.DependencyInjection.SharePointOnline.TUnit/SharePointServiceCollectionExtensionsTest.cs
+++ test/FredrikHr.Extensions.DependencyInjection.SharePointOnline.TUnit/SharePointServiceCollectionExtensionsTest.cs
@@ -1,6 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 
-namespace FredrikHr.MsExtDependencyInjection.SharePointOnline.TUnit;
+namespace FredrikHr.Extensions.DependencyInjection.SharePointOnline.TUnit;
 
 public class SharePointServiceCollectionExtensionsTest
 {
