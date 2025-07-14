7543fa4442775d092b778f452415eecf4853fcd5
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2025-07-14 21:32:00 +0200

    Improve MSAL Cache Helper Logging

--- src/FredrikHr.Extensions.DependencyInjection.Msal/MsalCacheHelperFactory.cs
+++ src/FredrikHr.Extensions.DependencyInjection.Msal/MsalCacheHelperFactory.cs
@@ -8,10 +8,12 @@ public sealed class MsalCacheHelperFactory :
     IDisposable
 {
     private readonly IOptionsMonitor<StorageCreationPropertiesBuilder> _storageCreationPropsOptions;
+    private readonly ILogger<MsalCacheHelper> _logger;
     private readonly IEnumerable<IConfigureOptions<MsalCacheHelper>> _setups;
     private readonly IEnumerable<IPostConfigureOptions<MsalCacheHelper>> _postConfigures;
     private readonly IValidateOptions<MsalCacheHelper>[] _validations;
     private readonly MsalCacheLoggerTraceListener _traceListener;
+    private readonly IDisposable? _loggerOptionsChangeRegistration;
 
     internal System.Diagnostics.TraceSource TraceSource { get; } =
         new(typeof(MsalCacheHelper).FullName!);
@@ -21,15 +23,21 @@ public sealed class MsalCacheHelperFactory :
         ILogger<MsalCacheHelper> logger,
         IEnumerable<IConfigureOptions<MsalCacheHelper>> setups,
         IEnumerable<IPostConfigureOptions<MsalCacheHelper>> postConfigures,
-        IEnumerable<IValidateOptions<MsalCacheHelper>> validations
+        IEnumerable<IValidateOptions<MsalCacheHelper>> validations,
+        IOptionsMonitor<LoggerFilterOptions> loggerOptionsMonitor
         )
     {
         _storageCreationPropsOptions = storageCreationPropsOptions;
+        _logger = logger;
         _setups = setups;
         _postConfigures = postConfigures;
         _validations = [.. validations ?? []];
         _traceListener = new MsalCacheLoggerTraceListener(logger);
         TraceSource.Listeners.Add(_traceListener);
+        _loggerOptionsChangeRegistration = loggerOptionsMonitor?.OnChange(
+            OnLoggerOptionsChanged
+            );
+        OnLoggerOptionsChanged(null);
     }
 
     Task<MsalCacheHelper> IOptionsFactory<Task<MsalCacheHelper>>.Create(string name)
@@ -80,8 +88,27 @@ public sealed class MsalCacheHelperFactory :
         return instance;
     }
 
+    private void OnLoggerOptionsChanged(LoggerFilterOptions? options)
+    {
+        if (_logger.IsEnabled(Microsoft.Extensions.Logging.LogLevel.Trace))
+            TraceSource.Switch.Level = System.Diagnostics.SourceLevels.All;
+        else if (_logger.IsEnabled(Microsoft.Extensions.Logging.LogLevel.Debug))
+            TraceSource.Switch.Level = System.Diagnostics.SourceLevels.Verbose;
+        else if (_logger.IsEnabled(Microsoft.Extensions.Logging.LogLevel.Information))
+            TraceSource.Switch.Level = System.Diagnostics.SourceLevels.Information;
+        else if (_logger.IsEnabled(Microsoft.Extensions.Logging.LogLevel.Warning))
+            TraceSource.Switch.Level = System.Diagnostics.SourceLevels.Warning;
+        else if (_logger.IsEnabled(Microsoft.Extensions.Logging.LogLevel.Error))
+            TraceSource.Switch.Level = System.Diagnostics.SourceLevels.Error;
+        else if (_logger.IsEnabled(Microsoft.Extensions.Logging.LogLevel.Critical))
+            TraceSource.Switch.Level = System.Diagnostics.SourceLevels.Critical;
+        else if (_logger.IsEnabled(Microsoft.Extensions.Logging.LogLevel.None))
+            TraceSource.Switch.Level = System.Diagnostics.SourceLevels.Off;
+    }
+
     public void Dispose()
     {
+        _loggerOptionsChangeRegistration?.Dispose();
         TraceSource.Listeners.Clear();
         _traceListener.Dispose();
     }
--- src/FredrikHr.Extensions.DependencyInjection.Msal/MsalCacheLoggerTraceListener.cs
+++ src/FredrikHr.Extensions.DependencyInjection.Msal/MsalCacheLoggerTraceListener.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Text;
 
 using Microsoft.Extensions.Logging;
 
@@ -8,6 +9,20 @@ internal sealed partial class MsalCacheLoggerTraceListener(
     ILogger<MsalCacheHelper> logger
     ) : TraceListener(logger.GetType().FullName!)
 {
+    private static readonly (TraceEventType eventType, Microsoft.Extensions.Logging.LogLevel logLevel)[] LogLevelMappings = [
+        (TraceEventType.Verbose, Microsoft.Extensions.Logging.LogLevel.Debug),
+        (TraceEventType.Information, Microsoft.Extensions.Logging.LogLevel.Information),
+        (TraceEventType.Warning, Microsoft.Extensions.Logging.LogLevel.Warning),
+        (TraceEventType.Error, Microsoft.Extensions.Logging.LogLevel.Error),
+        (TraceEventType.Critical, Microsoft.Extensions.Logging.LogLevel.Critical),
+        (~(TraceEventType.Verbose |
+            TraceEventType.Information |
+            TraceEventType.Warning |
+            TraceEventType.Error |
+            TraceEventType.Critical
+            ), Microsoft.Extensions.Logging.LogLevel.Trace),
+        ];
+    private readonly AsyncLocal<StringBuilder?> _writeBuffer = new();
     private readonly ILogger<MsalCacheHelper> _logger = logger;
 
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1848:Use the LoggerMessage delegates")]
@@ -15,21 +30,7 @@ internal sealed partial class MsalCacheLoggerTraceListener(
     [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0079: Remove unnecessary suppression", Justification = "erroneously triggered")]
     public override void TraceEvent(TraceEventCache? eventCache, string source, TraceEventType eventType, int id, string? format, params object?[]? args)
     {
-        Span<(TraceEventType eventType, Microsoft.Extensions.Logging.LogLevel logLevel)> mappings = stackalloc[]
-        {
-            (TraceEventType.Verbose, Microsoft.Extensions.Logging.LogLevel.Debug),
-            (TraceEventType.Information, Microsoft.Extensions.Logging.LogLevel.Information),
-            (TraceEventType.Warning, Microsoft.Extensions.Logging.LogLevel.Warning),
-            (TraceEventType.Error, Microsoft.Extensions.Logging.LogLevel.Error),
-            (TraceEventType.Critical, Microsoft.Extensions.Logging.LogLevel.Critical),
-            (~(TraceEventType.Verbose |
-                TraceEventType.Information |
-                TraceEventType.Warning |
-                TraceEventType.Error |
-                TraceEventType.Critical
-                ), Microsoft.Extensions.Logging.LogLevel.Trace),
-        };
-        foreach (ref readonly var tuple in mappings)
+        foreach (var tuple in LogLevelMappings)
         {
             if ((eventType & tuple.eventType) != default &&
                 _logger.IsEnabled(tuple.logLevel))
@@ -39,25 +40,42 @@ internal sealed partial class MsalCacheLoggerTraceListener(
         }
     }
 
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1848:Use the LoggerMessage delegates")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2254:Template should be a static expression")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0079: Remove unnecessary suppression", Justification = "erroneously triggered")]
+    public override void TraceEvent(TraceEventCache? eventCache, string source, TraceEventType eventType, int id, string? message)
+    {
+        foreach (var tuple in LogLevelMappings)
+        {
+            if ((eventType & tuple.eventType) != default &&
+                _logger.IsEnabled(tuple.logLevel))
+            {
+                _logger.Log(tuple.logLevel, id, $"{{Message}}", [message]);
+            }
+        }
+    }
+
     public override void Write(string? message)
     {
         if (message is null) return;
-        LogTraceWrite(message);
+        var writer = _writeBuffer.Value ??= new StringBuilder();
+        writer.Append(message);
     }
-
-    [LoggerMessage(EventId = 1, EventName = nameof(Write),
-        Level = Microsoft.Extensions.Logging.LogLevel.Trace,
-        Message = $"{{{nameof(message)}}}"
-        )]
-    private partial void LogTraceWrite(string message);
 
     public override void WriteLine(string? message)
     {
         if (message is null) return;
+        var writer = _writeBuffer.Value ??= new StringBuilder();
+        if (writer.Length > 0)
+        {
+            writer.Append(message);
+            message = writer.ToString();
+            _writeBuffer.Value = null;
+        }
         LogTraceWriteLine(message);
     }
 
-    [LoggerMessage(EventId = 2, EventName = nameof(WriteLine),
+    [LoggerMessage(EventId = 1, EventName = nameof(Write),
         Level = Microsoft.Extensions.Logging.LogLevel.Trace,
         Message = $"{{{nameof(message)}}}"
         )]
